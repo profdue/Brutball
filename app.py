e1b781459dcf1d1745d43b24fcc89f41e23655ea
profdue <34891100+profdue@users.noreply.github.com>
2025-12-18 15:59:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -709,7 +709,7 @@ def main():
                 template="plotly_white"
             )
             
-            st.plotly_chart(fig, use_container_width=True, use_container_width=True)
+            #st.plotly_chart(fig, use_container_width=True, use_container_width=True)
             
             # Match prediction interface
             st.markdown('<div class="sub-header">🎯 Match Analysis</div>', unsafe_allow_html=True)
